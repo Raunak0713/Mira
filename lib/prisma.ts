cd6b6774c70241b9074f603175a6e5821cf69cca
Raunak0713 <raunakbhanarkar7@gmail.com>
2024-12-15 21:40:39 +0530

    #8.1 Disable EsLint Rule

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
 declare global {
+  // Explicitly extend globalThis to include the 'prisma' property
   var prisma: PrismaClient | undefined;
 }
 
@@ -9,4 +10,3 @@ export const prisma = globalThis.prisma || new PrismaClient();
 if (process.env.NODE_ENV === 'development') {
   globalThis.prisma = prisma;
 }
- 
